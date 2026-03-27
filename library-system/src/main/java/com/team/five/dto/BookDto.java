637c5296314ef043c44c4cd69224626584bedf62
khsook6789 <khsook8995@gmail.com>
2026-03-27 19:31:30 +0900

    DTO 생성

--- library-system/src/main/java/com/team/five/dto/BookDto.java
+++ library-system/src/main/java/com/team/five/dto/BookDto.java
@@ -1,5 +1,20 @@
 package com.team.five.dto;
 
-public class BookDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+    private int bookId;
+    private String title;
+    private String author;
+    private int genreCode;
+    private String isbn;
 }
--- library-system/src/main/java/com/team/five/dto/Reservation.java
+++ library-system/src/main/java/com/team/five/dto/Reservation.java
@@ -1,5 +1,21 @@
 package com.team.five.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
 
+    private int reservationId;
+    private int userId;
+    private int bookId;
+    private String reservationDate;
 }
