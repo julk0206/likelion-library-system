c06668c01b5ee9cd4a98f247c5ec1b96b4f1109c
ABCganada <shinmg9712@gmail.com>
2026-03-30 11:09:22 +0900

    도서 조회 : 타입 검색

--- library-system/src/main/java/com/team/five/mapper/BookMapper.java
+++ library-system/src/main/java/com/team/five/mapper/BookMapper.java
@@ -1,10 +1,12 @@
 package com.team.five.mapper;
 
 import com.team.five.dto.BookDto;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface BookMapper {
 
-    public List<BookDto> findAllBooks();
+    List<BookDto> selectAllBooks();
+    List<BookDto> selectBooksByCondition(@Param("type") String type, @Param("value") String value);
 }
--- library-system/src/test/java/com/team/five/mapper/BookMapperTest.java
+++ library-system/src/test/java/com/team/five/mapper/BookMapperTest.java
@@ -1,35 +1,59 @@
 package com.team.five.mapper;
 
-import com.team.five.config.SqlSessionFactoryManager;
 import com.team.five.dto.BookDto;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import com.team.five.service.BookService;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class BookMapperTest {
 
-    private SqlSessionFactory factory;
-    private SqlSession session;
-    private BookMapper mapper;
+    private BookService bookService;
+    private Logger log = LoggerFactory.getLogger(BookMapperTest.class);
 
     @Before
     public void setup() {
-        factory = SqlSessionFactoryManager.getFactory();
-        session = factory.openSession();
-        mapper = session.getMapper(BookMapper.class);
+        bookService = new BookService();
     }
 
     @Test
-    public void findAllBooksTest() {
-        List<BookDto> bookDtoList = mapper.findAllBooks();
+    public void getAllBooksTest() {
+        List<BookDto> result = bookService.getAllBooks();
 
-        assertNotEquals(bookDtoList, 0);
+        assertNotEquals(result, 0);
     }
 
+    @Test
+    public void searchBooksTest() {
+        List<BookDto> result = bookService.searchBooks("TITLE", "자바");
+
+        assertNotEquals(result, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void searchBooksByInvalidConditionTest() {
+        List<BookDto> result = bookService.searchBooks("TEST", "TEST");
+    }
+
+    @Test
+    public void searchBooksNoResultTest() {
+        // DB에 절대 없을 법한 키워드로 검색
+        List<BookDto> result = bookService.searchBooks("TITLE", "@!#!@!#123456");
+
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void searchBooksEmptyValueTest() {
+        List<BookDto> result = bookService.searchBooks("TITLE", "");
+
+        assertNotNull(result);
+    }
 
 }
