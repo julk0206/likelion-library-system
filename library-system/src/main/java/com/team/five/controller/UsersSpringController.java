3a035009120e5f7ee6125cb406981f1a46e53a53
khsook6789 <khsook8995@gmail.com>
2026-04-27 22:13:23 +0900

    fix: UsersController(Servlet방식)과 URL충돌하는 문제 방지하기 위해 URL 수정

--- library-system/src/main/java/com/team/five/controller/UsersSpringController.java
+++ library-system/src/main/java/com/team/five/controller/UsersSpringController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users/spring")
+@RequestMapping("/spring/users")
 public class UsersSpringController {
 
     private final UsersService usersService;
