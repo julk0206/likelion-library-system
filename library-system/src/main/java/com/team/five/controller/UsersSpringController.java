538dec051ef25a07df962538acf839eec3129545
khsook6789 <khsook8995@gmail.com>
2026-04-27 20:54:56 +0900

    fix: URL 겹치는 문제 수정

--- library-system/src/main/java/com/team/five/controller/UsersSpringController.java
+++ library-system/src/main/java/com/team/five/controller/UsersSpringController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/users/spring")
 public class UsersSpringController {
 
     private final UsersService usersService;
