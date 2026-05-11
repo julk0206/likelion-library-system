0ef7c8721de666e8335267fe848b0d514edc9923
AmitaWhite <amitawhite75@gmail.com>
2026-05-11 14:41:03 +0900

    [#6] ✨ feat : 반납 관련 Mybatis 및 테스트 쿼리 정의

--- library-system/Scripts/Test_Script.sql
+++ library-system/Scripts/Test_Script.sql
@@ -9,7 +9,6 @@ SELECT * FROM GENRE;
 
 SELECT * FROM BOOK_ITEM;
 
-
 /* ------------------------------------------ */
 -- 반납 및 전체 흐름 제어
 
@@ -30,12 +29,9 @@ SELECT * FROM BOOK_ITEM;
 -- [Action_2 - 연체 도서 조회]
 -- [x]연체 도서 조회 (DUE_DATE 초과 미반납)
 
-
 -- 정리 Action => 유저 책 반납시
--- 해당 Book_id 에 대해 예약이 있으면? => 다음 사람에게 자동 대출(BOOK_ITEM.status 가 '대여가능' -> '대여중') 
+-- 해당 Book_id 에 대해 예약이 있으면? => 다음 사람에게 자동 대출(BOOK_ITEM.status 가 '대여가능' -> '대여중')
 -- 해당 BooK_id 에 대해 예약이 없으면? => 반납 처리(update RENT status, return_date)
-
-
 
 SELECT * FROM RENT;
 
@@ -46,6 +42,7 @@ SELECT * FROM RENT;
 --          - 예를 들어) 같은 이름을 가진 사람이 같은 제목의 책을 빌린 상태에서 반납
 -- 1.반납 처리
 -- 1-1 userID와 bookId 를 통해 RENT_ID 확인
+-- Mapped Method Name : getUnreturnedRentId
 SELECT RENT_ID
   FROM RENT r 
   JOIN BOOK_ITEM bi ON r.ITEM_ID = bi.ITEM_ID
@@ -53,20 +50,25 @@ SELECT RENT_ID
    AND r.USER_ID = #{userId}
    AND bi.BOOK_ID = #{bookId};-- RESULT : RENT_ID_RESULT
 -- 1-2 해당 REND_ID 를 통해 RENT 테이블 업데이트
+-- Mapped Method Name : updateBookAsReturning
 UPDATE RENT
    SET RETURN_DATE = sysdate
  WHERE RENT_ID = v$RENT_ID_RESULT;
 
 -- 3.BOOK_ITEM.STATUS 대여 가능 변경
 -- 3-1 RENT_ID 를 통해 다시 ITEM_ID 확인
-SELECT ITEM_ID
-  FROM RENT 
- WHERE RENT_ID = v$RENT_ID_RESULT; -- REULST : ITEM_ID_RESULT
+-- Mapped Method Name : getRentedItemId
+SELECT ITEM_ID FROM RENT WHERE RENT_ID = v$RENT_ID_RESULT;
+-- REULST : ITEM_ID_RESULT
 
 -- 3-2 ITEM_ID를 통해 BOOK_ITEM 업데이트
+-- Mapped Method Name : updateBookStatus
 UPDATE BOOK_ITEM
-   SET STATUS = '대여가능'
- WHERE STATUS = '대여중' AND ITEM_ID = v$ITEM_ID_RESULT;
+SET
+    STATUS = '대여가능'
+WHERE
+    STATUS = '대여중'
+    AND ITEM_ID = v$ITEM_ID_RESULT;
 
 /* -------------------Feature_11--------------- */
 -- 1. 2, 반납된 BOOK_ID 기준 예약 목록 조회 -> 수연님 구현 mapper id =selectReservationsByBookId
@@ -76,54 +78,68 @@ SELECT USER_ID , BOOK_ID, RESERVATION_ID
           FROM RESERVATION
          WHERE BOOK_ID = #{bookId}
            AND STATUS = '예약진행중')
- WHERE RN = 1; -- RESULT : RESERVATION_ID_RESULT , USER_ID_RESULT , BOOK_ID_RESULT
+ WHERE RN = 1;
+-- RESULT : RESERVATION_ID_RESULT , USER_ID_RESULT , BOOK_ID_RESULT
 
 -- 위를 FETCH FIRST 1 ROW ONLY 로 수정
+-- Mapped Method Name : getTopPriorityReservation
 SELECT USER_ID, BOOK_ID, RESERVATION_ID
   FROM RESERVATION
  WHERE BOOK_ID = #{bookId}
    AND STATUS = '예약진행중'
- ORDER BY SYSDATE ASC
+ ORDER BY RESERVATION_DATE ASC
  FETCH FIRST 1 ROW ONLY;
-
-
 
 -- 3.우선순위 1순위 예약에 대해 자동 RENT 생성
 -- 3-1 BOOK_ID_RESULT 를 통해 ITEM_ID_RESULT 생성 -> Action_10_3-1 결과 : v$ITEM_ID_RESULT 와 동일해야한다
+-- Mapped Method Name : getRentAvailableItemId
 SELECT ITEM_ID
-  FROM BOOK_ITEM
- WHERE STATUS = '대여가능' 
-   AND BOOK_ID = v$BOOK_ID_RESULT
-   AND ROWNUM=1; -- RESULT : v$ITEM_ID_RESULT_2
+FROM BOOK_ITEM
+WHERE
+    STATUS = '대여가능'
+    AND BOOK_ID = v$BOOK_ID_RESULT
+    AND ROWNUM = 1;
+-- RESULT : v$ITEM_ID_RESULT_2
 -- 3-2. v$USER_ID_RESULT , v$ITEM_ID_RESULT_2 를 통해 RENT 생성
 -- DUE_DATE 는 1주일로 한다.
-INSERT INTO RENT(USER_ID , ITEM_ID , DUE_DATE)
-    VALUES(v$USER_ID_RESULT, v$ITEM_ID_RESULT_2, SYSDATE+7);
+-- Mapped Method Name : inserRentRaw
+INSERT INTO
+    RENT (USER_ID, ITEM_ID, DUE_DATE)
+VALUES (
+        v$USER_ID_RESULT,
+        v$ITEM_ID_RESULT_2,
+        SYSDATE + 7
+    );
 
 -- 4. BOOK_ITEM.STATUS 업데이트
 UPDATE BOOK_ITEM
-   SET STATUS = '대여중'
- WHERE STATUS = '대여가능' AND v$ITEM_ID_RESULT_2;
+SET
+    STATUS = '대여중'
+WHERE
+    STATUS = '대여가능'
+    AND v$ITEM_ID_RESULT_2;
 
 -- 5.RESERVATION.STATUS - '대출완료' 변경
 UPDATE RESERVATION
-   SET STATUS = '대출완료'
- WHERE STATUS = '예약진행중' AND RESERVATION_ID = v$RESERVATION_ID_RESULT;
-
+SET
+    STATUS = '대출완료'
+WHERE
+    STATUS = '예약진행중'
+    AND RESERVATION_ID = v$RESERVATION_ID_RESULT;
 
 /* -------------------Action_20--------------- */
 -- input : 없음
 -- output : TITLE, ITEM_ID , 연체일, NAME , USER_ID
-SELECT b.TITLE, bi.ITEM_ID, u.NAME, u.USER_ID
-     , TRUNC(SYSDATE - r.DUE_DATE)  DELAY_DAYS
-  FROM RENT r 
-  JOIN BOOK_ITEM bi ON r.ITEM_ID = bi.ITEM_ID
-  JOIN BOOK b ON b.BOOK_ID = bi.BOOK_ID
-  JOIN USERS u ON r.USER_ID = u.USER_ID
- WHERE SYSDATE > r.DUE_DATE
-   AND bi.STATUS = '대여중'
-
-
+-- Mapped Method Name : getOverdueInfo
+SELECT b.TITLE, bi.ITEM_ID, u.NAME, u.USER_ID, TRUNC(SYSDATE - r.DUE_DATE) DELAY_DAYS
+FROM
+    RENT r
+    JOIN BOOK_ITEM bi ON r.ITEM_ID = bi.ITEM_ID
+    JOIN BOOK b ON b.BOOK_ID = bi.BOOK_ID
+    JOIN USERS u ON r.USER_ID = u.USER_ID
+WHERE
+    SYSDATE > r.DUE_DATE
+    AND bi.STATUS = '대여중';
 /* ------------------------------------------ */
 -- 5번
 -- 예약 관리 및 조회 API
@@ -138,13 +154,11 @@ SELECT b.TITLE, bi.ITEM_ID, u.NAME, u.USER_ID
 --
 -- RESERVATION.STATUS 변경 금지 (4번 전담)
 
-
-
 ---------------------------------------------------------------------
 /*                      EXTRA                                      */
 ---------------------------------------------------------------------
 /*
- reservation 테이블에
+reservation 테이블에
 status 컬럼을 추가하는게 좋을듯 싶은데
 의견을 여쭙고 싶어서요
 
@@ -161,28 +175,22 @@ status 컬럼을 추가하는게 좋을듯 싶은데
 
 */
 
-
 CREATE TABLE RESERVATION (
-    RESERVATION_ID   NUMBER     GENERATED ALWAYS AS IDENTITY,
-    USER_ID          NUMBER     NOT NULL,
-    BOOK_ID          NUMBER     NOT NULL,
-    RESERVATION_DATE DATE       DEFAULT SYSDATE NOT NULL,
-    STATUS           VARCHAR2(20) DEFAULT '예약진행중' NOT NULL,
+    RESERVATION_ID NUMBER GENERATED ALWAYS AS IDENTITY,
+    USER_ID NUMBER NOT NULL,
+    BOOK_ID NUMBER NOT NULL,
+    RESERVATION_DATE DATE DEFAULT SYSDATE NOT NULL,
+    STATUS VARCHAR2(20) DEFAULT '예약진행중' NOT NULL,
     CONSTRAINT PK_RESERVATION PRIMARY KEY (RESERVATION_ID),
-    CONSTRAINT FK_RESERV_USER FOREIGN KEY (USER_ID)
-        REFERENCES USERS (USER_ID),
-    CONSTRAINT FK_RESERV_BOOK FOREIGN KEY (BOOK_ID)
-        REFERENCES BOOK (BOOK_ID),
+    CONSTRAINT FK_RESERV_USER FOREIGN KEY (USER_ID) REFERENCES USERS (USER_ID),
+    CONSTRAINT FK_RESERV_BOOK FOREIGN KEY (BOOK_ID) REFERENCES BOOK (BOOK_ID),
     CONSTRAINT CHK_RESERVATION_STATUS CHECK (STATUS IN ('예약진행중', '대출완료'))
 );
 
-
 -- 5번(예약관리 및 조회 API) : 특정 Book_id 의 예약 존재 여부
- SELECT CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END
+SELECT CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END
             FROM RESERVATION
             WHERE BOOK_ID = #{bookId}
             AND STATUS = '예약진행중'
 
 ---------------------------------------------------------------------
-
-
